ebf8d80df7399cf5530cf092eaa45cd334119c2d
forevermickyy <mmquansah01@gmail.com>
2024-09-02 21:50:21 +0000

    first

---
+++ src/components/Signup.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Signup.css'
+import Crypto from '../assets/trade.png'
+const Signup = () => {
+  return (
+    <div className='signup'>
+        <div className='container'>
+            {/* left */}
+            <div>
+                <img src={Crypto} alt=''/>
+            </div>
+            {/* right */}
+            <div>
+                <h2>Earn passive income with crypto.</h2>
+                <p>Earn up to 12% annual rewards on 30+ digital assets.
+                 Simply hold your assets in the app to automatically 
+                 earn rewards at the end of each month with no lockups and no limits.</p>
+                 <div className='input-container'>
+                    <input type='email' placeholder='Enter your email'/>
+                    <button className='btn'>Learn More</button>
+                 </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Signup
